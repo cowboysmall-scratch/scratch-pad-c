2203ab0226df736e559f76be24d7afa08c4ec580
Jerry Kiely <jerry@cowboysmall.com>
2023-06-09 23:58:53 +0100

    added a string reverse util

---
+++ reverse/src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]) {
+
+    char reversed[100];
+
+    int length = strlen(argv[1]);
+
+    for (int i = 0; i < length; i++)
+        reversed[i] = argv[1][length - i - 1];
+
+    printf("\n");
+    printf("\tString reversed: %s\n", reversed);
+
+    return 0;
+}
